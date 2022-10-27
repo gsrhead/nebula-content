38b4b9c52454e0bbeb7bd3e31414f943c6ffed78
gsrhead <s2k9krpm00@gmail.com>
2022-10-27 09:20:51 -0400

    complete loop homework

---
+++ 6-loops/assignments/homework.js
@@ -0,0 +1,61 @@
+//   1.   Declare a for loop that starts at 0, goes until 30, and increments by 1
+//        Each loop it should log to the console 'the current index is _'
+   // Answer
+
+
+//   2.   Declare a for loop that starts at 1, goes until 10, and increments by 2
+//        Each loop it should log to the console the current index
+
+
+
+//   3.   Create an array with your top 5 celebrity names in it. 
+//        Be sure each element is a first & last name, ex: ['Martha Stewart', 'David Beckham', ...]
+//        Loop over the array and log each celebrity's full name to the console.
+
+
+
+//  4.   Loop over your celebrity list and 
+//       If a celebrity's full name has an even number of characters log it to the console
+
+
+
+//  5.   Use the celebrity array for this question
+//       Create a for loop which will iterate over each element and output a new array of only first names
+
+
+
+//  6.   Use the celebrity array for this question
+//       Create a for loop which will iterate over each element and output a new array of only last names
+
+
+
+//  7.   Loop over the celebrity list and then loop through each name. 
+//       If a letter is a vowel, log it to the console
+
+
+
+//   8.   Loop over the celebrity list and then loop through each name. 
+//        If a letter is uppercase, log it to the console
+
+
+
+//   9.   Convert your celebrity array to all caps and no spaces:
+//        Ex:
+//          Input:  ['Martha Stewart', 'David Beckham', etc..] 
+//          Output: ['MARTHA-STEWART', 'DAVID-BECKHAM', etc..]
+
+
+
+//  10.  Index your array to find your favorite celebrity. 
+//       Save that name to a variable
+//       Loop over the array until you find that individual
+//       Log that name to the console and break out of the loop
+
+
+
+// BONUS:
+//       Write a loop that iterates from zero until 30
+//       If an index is divisible by 2 log 'fizz'
+//       If an index is divisible by 3 log 'buzz'
+//       If an index is divisible by both 2 & 3, log 'fizz-buzz'
+//       Otherwise print the index to the console
