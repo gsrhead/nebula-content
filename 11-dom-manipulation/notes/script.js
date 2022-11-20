87aed3ac1c6570e13853abb54b998a2694eae261
gsrhead <s2k9krpm00@gmail.com>
2022-11-20 00:15:47 -0500

    dom practice assignment complete

---
+++ 11-dom-manipulation/notes/script.js
@@ -0,0 +1,23 @@
+// Grab the elements and log them to the console
+
+// Q1:  Attach this document to the index.html
+<link rel="script.js."/>;
+     <script src="script.js"> </script>;
+    <link rel="index.html"></link>;
+
+// Q2:  Grab the second element that has the text Hello
+document.getElementById("second");
+
+
+// Q3:  Grab the text in the h1 that says 'Good Afternoon'
+document.querySelector(".text h1");
+
+// Q4:  Grab the h1 with the text that says "Catch me if you can"
+document.querySelector("body > h1.catch-me");
+
+// Q5:  Grab the span element and change the text to "Got ya"
+document.querySelector("body > span");
+document.getElementById(i bet you cant get me).innerHTML = "got ya";
+let newEle = document.createElement('<span>');
+newEle.innerText = 'got ya';
+document.body.appendChild(newEle);
